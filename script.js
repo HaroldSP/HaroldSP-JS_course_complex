1e7ce9eba1c7eba246f866d585d0a7aef5a45ff6
HaroldSP <george.st.petersburg@gmail.com>
2023-02-04 00:19:46 +0300

    Lesson 05 HW (loops, prime munbers)

--- script.js
+++ script.js
@@ -4,13 +4,37 @@
 
 'strict';
 
-let foo = function (arg) {
-  if (arg === String(arg)) {
-    arg = arg.trim().slice(0, 30);
-    console.log(arg + '...');
-  } else {
-    alert('В коде ошибка. Функция "foo" принимает в качестве аргумента только строковый тип данных. Исправьте страницу и перезагрузите страницу');
+let arr = [];
+let checkIfStartsWith2or4 = false;
+
+const getRandomSevenDigitNumber = function () {
+  return String(Math.floor(1000000 + Math.random() * 9000000));
+}
+
+for (let i = 0; i < 7; i++) arr.unshift(getRandomSevenDigitNumber());
+
+for (let i = 0; i < arr.length; i++) {
+  if ((Math.floor(arr[i] / 1000000) == 2) || (Math.floor(arr[i] / 1000000)) == 4) {
+    console.log(arr[i], typeof arr[i]);
+    checkIfStartsWith2or4 = true;
   }
 }
 
-foo(' 012345678901234567890123456789тутначинаются символы 30+ '); // ожидаем вывод 012345678901234567890123456789...
+if (!checkIfStartsWith2or4) {
+  console.log(`В массиве ${arr} нет чисел, начинающихся на 2 или 4. Перезагрузите страницу.`)
+}
+
+const isPrime = function (number) {
+  if (number === 1) return false;
+
+  else if (number > 1) {
+    for (let i = 2; i < number; i++) {
+      if (number % i == 0) return false;
+    }
+    return true;
+  } else return false;
+}
+
+for (let i = 1; i < 100; i++) {
+  if (isPrime(i)) console.log(`${i} - простое число. Его делители 1 и ${i}`);
+}
