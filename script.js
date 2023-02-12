bb4c52e6abb69f75f58f61a9a597e9d2bbfaeb50
HaroldSP <george.st.petersburg@gmail.com>
2023-02-12 18:44:59 +0300

    start by pressing a button

--- script.js
+++ script.js
@@ -7,13 +7,51 @@
 
 // В input[type=text] можно ввести цвет: red, green, blue и так далее.
 // По нажатию на кнопку необходимо брать этот цвет и добавлять его свойству style="backgroundColor: " квадрата
-
 // Работать должно так: ввели в input[type=text] yellow, по нажатию на кнопку значение input[type=text] попадает в свойство style="backgroundColor: yellow"
 // и фон квадрата должен поменяться
 
-// 2) В кружке (который внутри квадрата) есть кнопка. Дать ей свойство style="display: none; "
+const btn = document.getElementById('btn');
 
-// 3) Повесить на input[type=range] обработчик события input и реализовать такой функционал:
+btn.onclick = function () {
+  const input = document.querySelector('input[type=text]');
+  const square = document.getElementById('square');
+  const span = document.getElementById('text-span');
+  const rangeSpan = document.getElementById('range-span');
+  const e_btn = document.getElementById('e_btn');
+  const range = document.getElementById('range');
+  const circle = document.getElementById('circle');
 
-// при каждом изменении положения ползунка значение input[type=range] необходимо заносить в свойства ширины и высоты кружка
-// (который внутри квадрата) (height и width) (в процентах!!)
+  input.placeholder = 'Цвета: red, green, blue etc...';
+  input.title = 'можно ввести цвет на английском языке: red, green, blue и так далее.';
+  span.innerHTML = 'Чтобы отправить цвет, нажмите "enter"';
+
+  const changeSquareColor = function (e) {
+    if (e.keyCode == 13) {
+      square.style.backgroundColor = input.value;
+      input.value = '';
+    }
+  }
+  input.addEventListener('keydown', changeSquareColor);
+
+  // 2) В кружке (который внутри квадрата) есть кнопка. Дать ей свойство style="display: none; "
+
+  // 3) Повесить на input[type=range] обработчик события input и реализовать такой функционал:
+
+  // при каждом изменении положения ползунка значение input[type=range] необходимо заносить в свойства ширины и высоты кружка
+  // (который внутри квадрата) (height и width) (в процентах!!)
+
+  rangeSpan.textContent = 50;
+
+  const getSpanValues = function (e) {
+    rangeSpan.textContent = e.target.value;
+  }
+
+  const getCircleSize = function () {
+    let values = rangeSpan.textContent;
+    circle.style.height = `${values}%`;
+    circle.style.width = `${values}%`;
+  }
+
+  range.addEventListener('input', getSpanValues);
+  range.addEventListener('change', getCircleSize);
+}
