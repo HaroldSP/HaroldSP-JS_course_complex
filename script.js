b46de1e9a98dbf163308d5858545278f4b7b95dc
HaroldSP <george.st.petersburg@gmail.com>
2023-02-17 01:15:04 +0300

    Les 16 ES6 (cities): task done

--- script.js
+++ script.js
@@ -10,3 +10,54 @@ const cityArr = {
   bel: ['Минск', 'Гомель', 'Могилёв', 'Витебск', 'Гродно', 'Брест'],
   jap: ['Токио', 'Киото', 'Осака', 'Иокогама']
 };
+
+const countrySelect = document.getElementById('country');
+const citySelect = document.getElementById('city');
+
+let targetArray = [];
+let selectedCountry = '';
+let selectedOnce = false;
+
+countrySelect.addEventListener('change', function () {
+  let option;
+
+  citySelect.style = 'display:block; position:absolute; top:8px; left:100px;'
+
+  if (selectedOnce) {
+    citySelect.innerHTML = '';
+  };
+
+  switch (this.value) {
+    case 'rus':
+      targetArray = cityArr.rus
+      break;
+    case 'uk':
+      targetArray = cityArr.uk
+      break;
+    case 'bel':
+      targetArray = cityArr.bel
+      break;
+    case 'jap':
+      targetArray = cityArr.jap
+      break;
+  };
+
+  selectedCountry = document.querySelector(`#country > option[value="${this.value}"]`).innerHTML;
+
+  for (let i = 0; i < targetArray.length; i++) {
+    option = document.createElement('option');
+    option.value = targetArray[i];
+    option.text = targetArray[i];
+    citySelect.appendChild(option);
+    selectedOnce = true;
+  };
+});
+
+// ////////////////////////////////////////////////////////////////////////////////////////////
+
+citySelect.addEventListener('change', function () {
+  let result = document.querySelector('.result');
+  let resultText = `${selectedCountry} ${this.value}`;
+
+  result.innerText = resultText;
+});
