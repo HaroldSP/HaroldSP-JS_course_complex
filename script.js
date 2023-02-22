7b96cf4498d33f2b6bb76639542aa2c5a84bd2d2
HaroldSP <george.st.petersburg@gmail.com>
2023-02-23 01:20:37 +0300

    debounced func done

--- script.js
+++ script.js
@@ -13,3 +13,22 @@
 // При этом каждый введённый пользователем в поле символ сбрасывает предыдущий отложенный вызов и запускает новый.
 
 // Таким образом программа должна ожидать завершения ввода пользователя и только после этого изменять текст в параграфе (тэг p)
+
+const input = document.getElementById('input');
+const output = document.getElementById('result');
+
+const debounce = (func, wait) => {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func(...args), wait); // вызывается оригинальная функция (ту, которую передали в аргумент)
+  };
+}
+
+const showResult = (event) => {
+  output.innerHTML = event.target.value;
+}
+
+const debouncedShowResult = debounce(showResult, 300);
+
+input.addEventListener('input', debouncedShowResult);
