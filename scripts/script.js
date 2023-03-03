5814a525f8dad012117ac7d7cc557ab908e4feb9
HaroldSP <george.st.petersburg@gmail.com>
2023-03-03 15:55:22 +0300

    les26 (ajax): task (start)

--- scripts/script.js
+++ scripts/script.js
@@ -5,65 +5,20 @@
 'use strict'
 
 /*
-На каждой клетке вы видите кнопки с направлением, напишите скрипт, чтобы при клике на кнопку, клетка менялась с соответствующей клеткой своим положением, соответственно направлению стрелки.
+1) Написать две функции для реализации AJAX запросов: getData и sendData
 
-Если вверх, вниз вправо или влево идти некуда, то происходить ничего не должно(со следующей строкой поменяться можно (например 20 с 21).
+3) Реализовать получение данных из файла .json (прикреплен под видео)  через функцию getData
 
-При клике на кнопку RESET все клетки должны вставать на свои места
+4) После получения объекта из файла .json должна произойти отправка данных (которые мы получили из файла .json) на URL через функцию sendData
 
-Лезть в вёрстку запрещено, использовать только те классы, что предоставлены
-Вызывать перезагрузку страницы запрещено, программа должна работать и сбрасываться без перезагрузок.
+https://jsonplaceholder.typicode.com/posts
+
+5) Ошибки должны быть обработаны
+
+При загрузке страницы сперва должно произойти получение данных из файла и после этого сразу отправка
+
+УСЛОЖНЕННОЕ ЗАДАНИЕ:
+
+Реализовать отправку данных на https://jsonplaceholder.typicode.com/posts методом XMLHttpRequest
+
 */
-const resetBtn = document.querySelector('.btn-reset');
-const blocks = document.querySelectorAll('.block');
-
-// навесим прослушку на весь элемент
-const container = document.querySelector('.square-body');
-container.addEventListener('click', handleBlockClick);
-
-// основная функция
-function handleBlockClick (event) {
-  // проверяем, был ли клик по стрелке
-  const clickedArrow = event.target.closest('.arrow');
-  if (!clickedArrow) return;
-
-  // у кликнутой стрелки есть класлист, в нем два класса, второй указывает на направление
-  const direction = clickedArrow.classList[1];
-
-  // находим ближайший к кликнутой стрелке блок и его номер
-  const block = event.target.closest('.block');
-  const blockNumber = block.querySelector('.block-number');
-
-  // превращаем нодлист блоков в обычный массив, чтобы применить indexOf() и найти индекс кликнутого блока
-  const currentIndex = Array.from(blocks).indexOf(block);
-
-  // в зависимости от кликнутой стрелки определим индекс блока, на который нужно перескочить
-  let targetIndex;
-  if (direction === 'left') {
-    targetIndex = currentIndex - 1;
-  } else if (direction === 'right') {
-    targetIndex = currentIndex + 1;
-  } else if (direction === 'top') {
-    targetIndex = currentIndex - 5;
-  } else if (direction === 'bottom') {
-    targetIndex = currentIndex + 5;
-  }
-
-  // проверим границы
-  if (targetIndex < 0 || targetIndex >= blocks.length) return;
-
-  const targetBlock = blocks[targetIndex];
-  const targetBlockNumber = targetBlock.querySelector('.block-number');
-
-  // поменяем местами номера
-  const temp = blockNumber.textContent;
-  blockNumber.textContent = targetBlockNumber.textContent;
-  targetBlockNumber.textContent = temp;
-}
-
-resetBtn.addEventListener('click', () => {
-  blocks.forEach((block, index) => {
-    const blockNumber = block.querySelector('.block-number');
-    blockNumber.textContent = index + 1;
-  });
-});
